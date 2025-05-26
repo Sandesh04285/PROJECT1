92521e9ac274dc25a90d63cf11551bdf0b5964ea
Sandesh04285 <choudharysandesh9@gmail.com>
2025-05-26 11:36:57 +0530

    Update website theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: 'rgb(var(--color-primary))',
-        secondary: 'rgb(var(--color-secondary))',
-        accent: 'rgb(var(--color-accent))',
-      },
-      backgroundColor: {
-        'custom-dark': '#121212',
-        'custom-darker': '#0a0a0a',
-      },
-      textColor: {
-        'glow-white': '#ffffff',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
